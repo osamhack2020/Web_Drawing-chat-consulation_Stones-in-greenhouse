76b55c574c6fe2644bddaf7a49ceab7def1cb0db
Dong Hyeon Sheen <donghyeonsheen@gmail.com>
2020-10-28 12:52:01 +0900

    Add files via upload

--- untact_counseling_system/js/chat_chating.js
+++ untact_counseling_system/js/chat_chating.js
@@ -116,7 +116,7 @@ let modalTestBtn = document.getElementById('modal__submit');
 
 
 function init() {
-    chatWS();
+    //chatWS();
     chatReadyClick();
     chatFinishClick();
     chatsendready();
@@ -293,6 +293,16 @@ function printSend(message) { //발신 메시지 출력
     chatBox.scrollTop = chatBox.scrollHeight;
 }
 
+function printSendPaint(message) { //그림 발신 메시지 출력
+    let contentBox = document.createElement('div');
+    let content = document.createElement('img');
+    content.src = message;
+    contentBox.classList.add('chats__block-send');
+    contentBox.appendChild(content);
+    chatBox.appendChild(contentBox);
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
 function chatWait() { //상담 정보 제출 후 대기
     COUN_READY.classList.add('vanish');
     COUN_FINISH.removeAttribute('class');
@@ -358,6 +368,129 @@ function chatsend() { //입력 메시지 발신
         }
     } 
 }
+
+//그림 전송
+
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const send = document.getElementById("jsSend");
+const cancel = document.getElementById("jsCancel");
+const show = document.getElementById("show__paint");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = "500";
+
+let paintPage = document.getElementById("chat__paint");
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "#ffffff";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR
+ctx.fillStyle = INITIAL_COLOR
+ctx.lineWidth = 2.5;
+
+let painting = false;
+let filling = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "그리기";
+    } else {
+        filling = true;
+        mode.innerText = "채우기";
+    }
+}
+
+function handleCanvasClick(){
+    if (filling){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    } else {
+
+    }
+}
+
+function handleSendClick(){
+    paintPage.removeAttribute('class');
+    let printData = canvas.toDataURL();
+    printSendPaint(printData);
+}
+
+function handleCancelClick(){
+    paintPage.removeAttribute('class');
+}
+
+function handleShowClick(){
+    paintPage.classList.add('paint__show')
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(send){
+    send.addEventListener("click", handleSendClick);
+}
+
+if(cancel){
+    cancel.addEventListener("click", handleCancelClick);
+}
+
+if(show){
+    show.addEventListener("click", handleShowClick);
+}
+
+
 
 //로그 조회 데모
 
